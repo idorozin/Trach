35f35f91af882a208a28ae6805eaf5d2b4ba8852
ido <rozin.ido@gmail.com>
2021-08-28 11:42:49 +0300

    FastCar TragetIndicator
    *fix tragetindicator is not initialized during scene reload

--- Assets/Scripts/Obstacles/FastCar.cs
+++ Assets/Scripts/Obstacles/FastCar.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,15 +14,29 @@ public class FastCar : MonoBehaviour
     [SerializeField]
     private float viewDistance;
     private Rigidbody rb;
+    [SerializeField] private GameObject exclamationMark;
+
+    private GameObject canvas_;
+    private GameObject mark = null;
+    public Canvas canvas;
+    private bool done;
+
+    private TargetIndicator targetIndicator;
+
 
     void Start()
     {
+        Debug.Log("fast car start");
+        canvas_ = GameObject.Find("ScoreCanvas");
+        canvas = canvas_.GetComponent<Canvas>();
         rb = gameObject.GetComponent<Rigidbody>();
+        targetIndicator = Instantiate(exclamationMark, canvas.transform).GetComponent<TargetIndicator>();
+        targetIndicator.InitialiseTargetIndicator(gameObject, Camera.main, canvas);
     } 
 
     void Update(){
-
-
+        
+        NotifyPlayer();
         if(transform.position.z > GameManager.Instance.playerpos.position.z)
             rb.constraints = RigidbodyConstraints.None;
     }
@@ -38,5 +54,34 @@ public class FastCar : MonoBehaviour
             if (lives == 0)
                 obs.Destroy();
         }
+    }
+
+    void NotifyPlayer()
+    {
+        
+        if (done)
+            return;
+        
+        /*
+        if (GameManager.Instance.playerpos.position.z - transform.position.z < 0.1f)
+        {
+            done = true;
+            targetIndicator.DisableIndicator()
+        }
+        */
+
+
+        if (GameManager.Instance.playerpos.position.z - transform.position.z < 100)
+        {
+            targetIndicator.UpdateTargetIndicator();
+
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (targetIndicator == null)
+            return;
+        targetIndicator.gameObject.SetActive(false);
     }
 }
