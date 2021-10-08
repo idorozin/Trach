61077f17b191a56608e078ba94152720776b964a
ido <rozin.ido@gmail.com>
2021-10-08 09:14:05 +0300

    Build fix

--- Assets/Scripts/Coins/CoinSpawner.cs
+++ Assets/Scripts/Coins/CoinSpawner.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows.Speech;
 using Random = UnityEngine.Random;
 
 public class CoinSpawner : MonoBehaviour
