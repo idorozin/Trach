9259c1a3183560b23c2fe6b6b1d8b720e9c54ab7
haktimel <rozin.ido@gmail.com>
2022-12-02 15:57:51 +0200

    infinite floor and borders

--- Assets/Scripts/Terrain/TileManger.cs
+++ Assets/Scripts/Terrain/TileManger.cs
@@ -9,12 +9,14 @@ using Random = UnityEngine.Random;
 public class TileManger : MonoBehaviour
 {
 
-    
+    [SerializeField] private Transform[] floors;
+
+    [SerializeField] private Transform[] borders;
 
     [SerializeField]
     private string[] prefabs;
 
-    private float currentZ;
+    private float currentZ = 0;
 
     [SerializeField]
     private Vector3 startPos;
@@ -44,36 +46,62 @@ public class TileManger : MonoBehaviour
         SpawnTile(false);
         SpawnTile(false);
         SpawnTile(false);
+        currentBorderZ += 500;
 
     }
+    [SerializeField]
+    private float floorCurrentZ;
+    [SerializeField]
+    private float currentBorderZ;
+
+    private int currentFloorZ = 1900;
 
     void Update()
     {
         foreach (var tile in tiles)
         {
 
-            if (tile.Item2.position.z + tileLength/2f+40f < player.position.z)
+            if (tile.Item2.position.z + tileLength / 2f + 40f < player.position.z)
             {
-                PlaceInFront(tile.Item1);
+                PlaceInFront(tile.Item1, tileLength,ref currentZ);
             }
         }
 
+        foreach (var floor in floors)
+        {
+            if (floor.position.z + 2400 < player.position.z)
+            {
+                floor.gameObject.transform.position = new Vector3(floor.transform.position.x,
+                    floor.transform.position.y, currentFloorZ);
+                currentFloorZ += 1900;
+            }
+        }
+        bool borderMoved = false;
+        foreach (var border in borders)
+        {
+            if (border.position.z + 700 < player.position.z)
+            {
+                //currentBorderZ += 500;
+                borderMoved = true;
+                border.gameObject.transform.position = new Vector3(border.transform.position.x, border.transform.position.y, currentBorderZ);
+            }
+            
+        }
+
+        if (borderMoved)
+            currentBorderZ += 500;
     }
 
     void SpawnTile(bool moveBorder = true)
     {
         GameObject go = ObjectPool.Instance.GetObject(prefabs[Random.Range(0, prefabs.Length)]);
         tiles.Add(Tuple.Create(go,go.transform));
-        PlaceInFront(go , moveBorder);
+        PlaceInFront(go ,tileLength, ref currentZ);
     }
 
-    void PlaceInFront(GameObject go , bool moveBorder = true)
+    void PlaceInFront(GameObject go , float objectLength,ref float currentZ)
     {
         go.transform.position = new Vector3(startPos.x,startPos.y,startPos.z + currentZ);
-        currentZ += tileLength;
-		if(moveBorder){
-        borderLeft.transform.position = new Vector3(borderLeft.transform.position.x,borderLeft.transform.position.y,borderLeft.transform.position.z + tileLength/2);
-        borderRight.transform.position = new Vector3(borderRight.transform.position.x,borderRight.transform.position.y,borderRight.transform.position.z + tileLength/2);
-		}
+        currentZ += objectLength;
     }
 }
